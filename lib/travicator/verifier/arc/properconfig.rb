aad1ed206204780fff123414a8c205f3420e0385
Austen McDonald <austenmc@fb.com>
2014-06-15 22:46:51 -0700

    Add verifier that checks .arcconfig for phabricator.uri

---
+++ lib/travicator/verifier/arc/properconfig.rb
@@ -0,0 +1,46 @@
+require "travicator/verifier/base"
+
+require "json"
+
+module Travicator::Verifier::Arc
+  class ProperConfig < Travicator::Verifier::Base
+    def self.options
+      { :directory => Dir.pwd,
+        :config => Travicator.config }
+    end
+
+    def self.deps
+      [ Travicator::Verifier::Arc::ArcConfig.class ]
+    end
+
+    def run
+      raise "Verifier can only be run once" unless @state == Travicator::Verifier::UNKNOWN
+
+      @state = Travicator::Verifier::RUNNING
+      # Check if options[:directory] contains an .arcconfig with the proper
+      # configuration values.
+      filename = options[:directory] + '/.arcconfig'
+      begin
+        hash = JSON.parse(IO.read(filename))
+      rescue
+        @state = Travicator::Verifier::FAILURE
+        @error = "Looks like your .arcconfig is not valid JSON."
+        return
+      end
+
+      unless hash["phabricator.uri"] == options[:config]["phabricator.uri"]
+        @state = Travicator::Verifier::FAILURE
+        @error = "phabricator.uri setting in .arcconfig doesn't match travicator config: expected #{options[:config][:phabricator_uri]} instead of #{hash["phabricator.uri"]}."
+        return
+      end
+
+      unless hash["arcanist_configuration"] == "TravicatorArcanistConfiguration"
+        @state = Travicator::Verifier::FAILURE
+        @error = "arcanist_configuration setting in .arcconfig doesn't specify the proper php class config: expected TravicatorArcanistConfiguration instead of #{hash["arcanist_configuration"]}."
+        return
+      end
+      
+      @state = Travicator::Verifier::SUCCESS
+    end
+  end
+end
---
+++ spec/verifier/arc/properconfig_spec.rb
@@ -0,0 +1,63 @@
+require "travicator/verifier"
+require "travicator/verifier/arc/properconfig"
+
+require "tmpdir"
+require "json"
+
+module Travicator::Verifier::Arc
+  describe ProperConfig do
+    it "detects a properly setup .arcconfig" do
+      uri = "http://trythis"
+      Dir.mktmpdir { |tmp|
+        Dir.chdir(tmp) do
+          IO.write('.arcconfig', {
+            "phabricator.uri" => uri,
+            "arcanist_configuration" => "TravicatorArcanistConfiguration"
+          }.to_json)
+        end
+        v = ProperConfig.new({ :directory => tmp, :config => { "phabricator.uri" => uri } } )
+        v.run()
+        expect(v.state).to be(Travicator::Verifier::SUCCESS)
+        expect(v.error).to be_nil
+      }
+    end
+    it "detects an improper phabricator.uri" do
+      uri = "http://trythis"
+      Dir.mktmpdir { |tmp|
+        Dir.chdir(tmp) do
+          IO.write('.arcconfig', {
+            "phabricator.uri" => "http://trythat",
+            "arcanist_configuration" => "TravicatorArcanistConfiguration"
+          }.to_json)
+        end
+        v = ProperConfig.new({ :directory => tmp, :config => { "phabricator.uri" => uri } } )
+        v.run()
+        expect(v.state).to be(Travicator::Verifier::FAILURE)
+        expect(v.error).to be_a(String)
+      }
+    end
+    it "detects an improper arcanist_configuration" do
+      uri = "http://trythis"
+      Dir.mktmpdir { |tmp|
+        Dir.chdir(tmp) do
+          IO.write('.arcconfig', {
+            "phabricator.uri" => uri,
+            "arcanist_configuration" => "something wrong"
+          }.to_json)
+        end
+        v = ProperConfig.new({ :directory => tmp, :config => { "phabricator.uri" => uri } } )
+        v.run()
+        expect(v.state).to be(Travicator::Verifier::FAILURE)
+        expect(v.error).to be_a(String)
+      }
+    end
+    it "cannot be run twice" do
+      uri = "http://trythis"
+      Dir.mktmpdir { |tmp|
+        v = ProperConfig.new({ :directory => tmp, :config => { "phabricator.uri" => uri } } )
+        v.run()
+        expect { v.run() }.to raise_error
+      }
+    end
+  end
+end
