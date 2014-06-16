faefc2fdc998c26e6baf74f893ca9e4956c7844e
Austen McDonald <austenmc@fb.com>
2014-06-15 23:53:30 -0700

    Make help command actually work

--- lib/travicator/command/help.rb
+++ lib/travicator/command/help.rb
@@ -1,6 +1,19 @@
+require 'travicator'
 require 'travicator/command/base'
+
 class Travicator::Command::Help < Travicator::Command::Base
   def run
-    puts "Running #{self.command} with options #{self.options}"
+    puts "Usage: travicator COMMAND\n\n"
+
+    puts "Available commands:\n"
+    commands = Travicator::Command.commands.sort_by { |cmd, klass| cmd }
+
+    for cmd, klass in commands
+      puts "    #{cmd.ljust(12)} #{klass.help}\n"
+    end
+  end
+
+  def self.help
+    "display this help message"
   end
 end
--- lib/travicator/command/version.rb
+++ lib/travicator/command/version.rb
@@ -5,4 +5,8 @@ class Travicator::Command::Version < Travicator::Command::Base
   def run
     puts Travicator::VERSION
   end
+
+  def self.help
+    "display version"
+   end 
 end
