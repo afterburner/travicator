206df68b2b5de3dc3bb8f5556c62a77fc7c822ce
Austen McDonald <austenmc@fb.com>
2014-06-25 22:31:47 -0700

    Add (empty) verify command

---
+++ lib/travicator/command/verify.rb
@@ -0,0 +1,10 @@
+require 'travicator/command/base'
+
+class Travicator::Command::Verify < Travicator::Command::Base
+  def run
+  end
+
+  def self.help
+    "check the current repo for proper configuration"
+  end
+end
